f6031b6c19140f68ee6cf0d22acfe778e944d5b5
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-05-25 14:42:38 -0300

    first commit

---
+++ FarfaleDelas/FarfaleDelasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarfaleDelasApp.swift
+//  FarfaleDelas
+//
+//  Created by Lucas Migge de Barros on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FarfaleDelasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
